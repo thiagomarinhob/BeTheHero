fdd5b637b602222d108cee3299eced196aafa91b
Thiago Marinho <thiagomarinhobezerra@gmail.com>
2020-03-24 17:19:42 -0300

    Concluindo o Backend

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,12 +1,22 @@
 const express = require('express')
 
+const OngController = require('./controllers/OngController')
+const incidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
+
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-    return response.json({
-        evento: 'Semana 11',
-        aluno: 'Thiago'
-    })
-})
+routes.post('/sessions', SessionController.store)
+
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.store)
+
+routes.get('/profile', ProfileController.index)
+
+routes.post('/incidents', incidentController.store)
+routes.get('/incidents', incidentController.index)
+routes.delete('/incidents/:id', incidentController.delete)
+
 
 module.exports = routes;
